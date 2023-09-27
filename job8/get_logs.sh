3ecbe43a613c7742c0035cab2545cd2068e7e683
root <root@Gnocchi>
2023-09-27 15:03:23 +0200

    job8

---
+++ job8/get_logs.sh
@@ -0,0 +1,7 @@
+connection_number=$(grep "prat" /var/log/auth.log | wc -l)
+
+date=$(date +"%d-%m-%Y-%H:%M")
+
+echo "$connection_number : $date" > "backup/number_connection-$date.txt"
+
+tar  -czf "backup/number_connection-$date.tar" "backup/number_connection-$date.txt"
